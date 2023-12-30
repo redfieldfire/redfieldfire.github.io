5e97f074a36bb051f7712ed25312546c5752db6c
Christopher Serna Flores <cserna@controlla.com.mx>
2023-12-30 07:01:25 -0600

    size

--- script.js
+++ script.js
@@ -1,68 +1,107 @@
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
-const size = parseInt(prompt("size", "30"))
+const size = parseInt(prompt("size max(30) min(20)", "30"));
 
-const width = parseInt(prompt("width", "30"))
-const height = parseInt(prompt("height", "10"))
+const width = parseInt(prompt("width max(40)", "40"));
+const height = parseInt(prompt("height max(20)", "20"));
 
-container.style.width = (width * (size + 2)) + "px"
-container.style.height = (height * (size + 1)) + "px"
+container.style.width = (width * (size + 2)) + "px";
+container.style.height = (height * (size + 2)) + "px";
 
-var quantity = 0
-var color = "#000"
+var quantity = 0;
+var color = "#FFF";
+var drag = false;
 
-forEachAll(addLabel)
+document.body.style.setProperty('--size-square', size + "px")
 
+forEachAll(addSquares);
+
+//Thats map all the squares and affect all with one action
 function forEachAll(action) {
-    quantity = width * height
+    quantity = width * height;
     while (quantity > 0) {
-        quantity--
-        action(quantity)
+        quantity--;
+        action(quantity);
     }
 }
 
-function addLabel(id) {
-    container.innerHTML += '<div id='+id+' onmouseover="addHover('+id+')" onmouseout="removeHover('+id+')" onclick="paint('+id+')" class="square"></div>'
+//Create all the squares
+function addSquares(id) {
+    container.innerHTML += square(id);
+}
+
+function square(id) {
+    return (
+        '<div id=' + id + ' ' +
+        'draggable="false" ' +
+        'onmouseover="addHover(' + id + ')" ' +
+        'onmouseout="removeHoverAndDrag(' + id + ')" ' +
+        'onmousedown="turnOnDrag()" ' +
+        'onmouseup="turnOffDrag()" ' +
+        'onmousemove="checkDrag(' + id + ')" ' +
+        'onclick="paint(' + id + ')" ' +
+        'class="square"></div>'
+    );
 }
 
 function addHover(id) {
-    document.getElementById(id).classList.add('hover-square')
+    //Temporal CSS for hover and paint the saquare with the selected color
+    document.getElementById(id).classList.add("hover-square");
 }
 
-function removeHover(id) {
-    document.getElementById(id).classList.remove('hover-square')
+function removeHoverAndDrag(id) {
+    //Remove the hover, and check if drag is active
+    document.getElementById(id).classList.remove("hover-square");
+    checkDrag(id);
+}
+
+function checkDrag(id) {
+    if (drag) paint(id)
 }
 
 function paint(id) {
-    document.getElementById(id).style.backgroundColor = color
+    //Paint the saquare
+    document.getElementById(id).style.backgroundColor = color;
+}
+
+function turnOnDrag() {
+    drag = true
+}
+
+function turnOffDrag() {
+    drag = false
 }
 
 function changeColor(newColor) {
-    color = newColor
-    document.body.style.setProperty('--color-hover', newColor)
-    console.log(document.body.style)
+    //Here gets the new color, and put the variable in CSS for the hover effects
+    color = newColor;
+    document.body.style.setProperty("--color-hover", newColor);
 }
 
 function clearAll() {
-    forEachAll(clearOne)
-}
+    //Erase all the squares
+    forEachAll(clearOne);
+}   
 
 function clearOne(id) {
-    document.getElementById(id).style.backgroundColor = "black"
+    document.getElementById(id).style.backgroundColor = "black";
 }
 
-function showLines(){
-    forEachAll(paintBorder)
+//The next 4 is for show and hide the lines of the squares
+
+function showLines() {
+    forEachAll(showBorder);
 }
 
-function paintBorder(id) {
-    document.getElementById(id).style.border = "solid 1px rgba(255, 255, 255, 0.2)"
+function showBorder(id) {
+    document.getElementById(id).style.border =
+        "solid 1px rgba(255, 255, 255, 0.2)";
 }
 
-function hideLines(){
-    forEachAll(hideLine)
+function hideLines() {
+    forEachAll(hideBorder);
 }
 
-function hideLine(id){
-    document.getElementById(id).style.border = "solid 1px transparent"
+function hideBorder(id) {
+    document.getElementById(id).style.border = "solid 1px transparent";
 }
